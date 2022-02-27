0c941a5072a10802c095cf02116bc01792bf1e5f
wewilliams9999 <williamswallise@gmail.com>
2022-02-26 19:18:30 -0600

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Subaru";
+            myCar.Model = "Crosstrek";
+            myCar.Year = 2019;
+
+            Console.WriteLine($"My car is a {myCar.Year} {myCar.Make} {myCar.Model}");
+        
+        }
+    }
+}
